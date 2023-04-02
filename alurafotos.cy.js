7b0b3c90e93e817d93274c30a8abc724899490c1
Caucasa07 <129119559+Caucasa07@users.noreply.github.com>
2023-04-01 23:32:01 -0300

    Add files via upload

--- alurafotos.cy.js
+++ alurafotos.cy.js
@@ -3,7 +3,17 @@ describe('Registro de usuario', ()=>{
         cy.visit('https://alura-fotos.herokuapp.com')
     })
 
-    it ('verificar mensagens de validação', ()=> {
+    it('verificar mensagens de validação de login', ()=> {
+        cy.contains('ap-vmessage', 'User name is required!').should('be.visible');
+        cy.contains('ap-vmessage', 'Password is required!').should('be.visible');
+    })
+
+    it('verificar mensagens de validação Registro ', ()=> {
+        cy.get('input[formcontrolname="userName"]').type('souqa');
+        cy.contains('button', 'login').click();
+    })
+
+    it ('verificar mensagens de validação Registro', ()=> {
         cy.contains('a', 'Register now').click();
         cy.contains('button', 'Register').click();
         cy.contains('ap-vmessage', 'Email is required').should('be.visible');
@@ -12,19 +22,12 @@ describe('Registro de usuario', ()=>{
         cy.contains('ap-vmessage', 'User name is required').should('be.visible');
         cy.contains('ap-vmessage', 'Password is required').should('be.visible');
     })
-    it('verificar vali email', ()=> {
+    it('verificar validação email', ()=> {
         cy.contains('a', 'Register now').click();
         cy.contains('button', 'Register').click();
         cy.contains('ap-vmessage', 'Email is required').should('be.visible');
         cy.get('input[formcontrolname="email"]').type('souqa');
         cy.contains('ap-vmessage', 'Invalid e-mail').should('be.visible');
-    })
-
-    it.only('verificar minimo de caracteres no nome do usuario', ()=>{
-        cy.contains('a', 'Register now').click();
-        cy.contains('button', 'Register').click();
-        cy.get('input[formcontrolname="fullName"]').type('a');
-        cy.contains('ap-vmessage', 'Mininum length is 2').should('be.visible');
     })
         
 })
